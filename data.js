f6c46ecf037b087e2aa68500a222156e81b17508
Imane Key <imane.key96@gmail.com>
2024-11-15 14:02:21 -0500

    data

---
+++ data.js
@@ -0,0 +1,37 @@
+// Team Members Data
+const teamMembers = [
+    {
+      id: 1,
+      name: "Aloro Lee",
+      role: "Frontend Developer",
+      skills: ["HTML", "CSS", "JavaScript", "React"],
+    },
+    {
+      id: 2,
+      name: "Imane Key",
+      role: "Backend Developer",
+      skills: ["Node.js", "Express", "MongoDB", "SQL"],
+    },
+    {
+      id: 3,
+      name: "Cayla Wanzer",
+      role: "UI/UX Designer",
+      skills: ["Figma", "Sketch", "Adobe XD", "Prototyping"],
+    },
+    {
+      id: 4,
+      name: "Herbert Dunbar",
+      role: "Project Manager",
+      skills: ["Agile", "Scrum", "Leadership"],
+    },
+
+    id: 5,
+    name: "Kay Mcray",
+    role: "Softwar Engineer",
+    skills: ["Agile", "Scrum", "Github"],
+  },
+  ];
+  
+
+  export { teamMembers, products };
+  
